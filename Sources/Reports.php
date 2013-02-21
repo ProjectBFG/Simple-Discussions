42127b6f6e9cc4ba1bf0b383e026c439700a6b94
Antes <malinbay@gmail.com>
2013-02-21 17:27:08 +0200

    Print Page Signed-off-by: Antes <malinbay@gmail.com>

--- Sources/Reports.php
+++ Sources/Reports.php
@@ -86,20 +86,11 @@ function ReportsMain()
 		'main' => array(
 			'layers' => null,
 		),
-		'print' => array(
-			'layers' => array('print'),
-		),
 	);
 
 	// Specific template? Use that instead of main!
 	if (isset($_REQUEST['st']) && isset($reportTemplates[$_REQUEST['st']]))
-	{
 		$context['sub_template'] = $_REQUEST['st'];
-
-		// Are we disabling the other layers - print friendly for example?
-		if ($reportTemplates[$_REQUEST['st']]['layers'] !== null)
-			$context['template_layers'] = $reportTemplates[$_REQUEST['st']]['layers'];
-	}
 
 	// Make the page title more descriptive.
 	$context['page_title'] .= ' - ' . (isset($txt['gr_type_' . $context['report_type']]) ? $txt['gr_type_' . $context['report_type']] : $context['report_type']);
